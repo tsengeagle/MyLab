421895b4b1401ae767de2ba3c8c6d6c605efe53a
tsengeagle <tsengeagle@gmail.com>
2016-12-30 17:16:04 +0800

    Lab

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Status = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.YM = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Hosp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CostID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Status,
+            this.YM,
+            this.Hosp,
+            this.CostID,
+            this.Value});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 88);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(642, 342);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Status
+            // 
+            this.Status.DataPropertyName = "Status";
+            this.Status.FalseValue = "False";
+            this.Status.HeaderText = "狀態";
+            this.Status.Name = "Status";
+            this.Status.TrueValue = "True";
+            // 
+            // YM
+            // 
+            this.YM.DataPropertyName = "YM";
+            this.YM.HeaderText = "年月";
+            this.YM.Name = "YM";
+            // 
+            // Hosp
+            // 
+            this.Hosp.DataPropertyName = "Hosp";
+            this.Hosp.HeaderText = "院區";
+            this.Hosp.Name = "Hosp";
+            // 
+            // CostID
+            // 
+            this.CostID.DataPropertyName = "CostID";
+            this.CostID.HeaderText = "成本中心";
+            this.CostID.Name = "CostID";
+            // 
+            // Value
+            // 
+            this.Value.DataPropertyName = "Value";
+            this.Value.HeaderText = "金額";
+            this.Value.Name = "Value";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 442);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Status;
+        private System.Windows.Forms.DataGridViewTextBoxColumn YM;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Hosp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CostID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var list = new List<MyType>()
+            {
+                new MyType() {
+                    Status=true,
+                Hosp="HL",
+                YM="10510",
+                CostID="000001",
+                Value="10000"},
+                new MyType() {
+                    Status=false,
+                Hosp="HL",
+                YM="10510",
+                CostID="000002",
+                Value="10000"},
+            };
+
+            dataGridView1.DataSource = list;
+        }
+    }
+
+    internal class MyType
+    {
+        public string Value { get; set; }
+        public string CostID { get; set; }
+        public string YM { get; internal set; }
+        public string Hosp { get; set; }
+        public bool Status { get; set; }
+    }
+}
